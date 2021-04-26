b89f1b25033f565f238656b2bb06ad6854f6f79f
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2021-04-26 11:12:27 +0300

    Test MVP functionality of country wc visualizer

--- src/dash-test.py
+++ src/dash-test.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 from wordcloud import WordCloud
@@ -36,25 +37,6 @@ reut_country_geo_topic.set_index('country', inplace=True)
 reut_country_geo_topic['topiccounts'] = reut_country_geo_topic['topiccounts'].apply(
     eval)
 
-finland_dict = reut_country_geo_topic.loc['FINLAND']['topiccounts']
-
-finland = plt.figure(figsize=(10, 10))
-ax = finland.add_axes([0, 0, 1, 1])
-ax.axis('off')
-
-ax.margins(0)
-reut_country_geo_topic.loc[['FINLAND']].plot('count', ax=ax)
-plt.savefig('pics/finland.png', bbox_inches="tight", pad_inches=0)
-
-finland_mask = np.array(Image.open("pics/finland.png"))
-finland_cloud = WordCloud(background_color="white", mask=finland_mask,
-                          width=1000, height=1000, contour_width=0.5)
-finland_cloud.generate_from_frequencies(finland_dict)
-
-wc_img = finland_cloud.to_image()
-with BytesIO() as buffer:
-    wc_img.save(buffer, 'png')
-    img2 = base64.b64encode(buffer.getvalue()).decode()
 
 app.layout = html.Div(children=[
     html.H1(children='Reuters topic wordclouds by countries', style={
@@ -63,11 +45,42 @@ app.layout = html.Div(children=[
     }),
   html.Label('Dropdown'),
     dcc.Dropdown(
+        id='dropdown',
         options= drop_down_options,
         value='MTL'
     ),
-    html.Img(src="data:image/png;base64," + img2),
+    html.Img(id='cloud')
 ])
+
+@app.callback(
+    Output(component_id='cloud', component_property='src'),
+    Input('dropdown', 'value'))
+
+def update_cloud(selected_country):
+    country_dict = reut_country_geo_topic.loc['FINLAND']['topiccounts']
+
+    country_fig = plt.figure(figsize=(10, 10))
+    ax = country_fig.add_axes([0, 0, 1, 1])
+    ax.axis('off')
+    ax.margins(0)
+
+    reut_country_geo_topic.loc[[selected_country]].plot('count', ax=ax)
+    plt.savefig(f'pics/{selected_country}.png', bbox_inches="tight", pad_inches=0)
+
+    country_mask = np.array(Image.open(f'pics/{selected_country}.png'))
+    country_cloud = WordCloud(background_color="white", mask=country_mask,
+                            width=1000, height=1000, contour_width=0.5)
+    country_cloud.generate_from_frequencies(country_dict)
+
+    wc_img = country_cloud.to_image()
+    with BytesIO() as buffer:
+        wc_img.save(buffer, 'png')
+        fig = base64.b64encode(buffer.getvalue()).decode()
+
+    src="data:image/png;base64," + fig
+
+    return src
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
